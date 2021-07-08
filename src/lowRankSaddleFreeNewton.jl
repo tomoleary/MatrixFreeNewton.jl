671b69f73a6277b54f1499f858f754b3337c51a5
Tom O'Leary-Roseberry <tom.olearyroseberry@gmail.com>
2021-07-08 10:58:54 -0500

    adding license header

--- src/lowRankSaddleFreeNewton.jl
+++ src/lowRankSaddleFreeNewton.jl
@@ -1,3 +1,20 @@
+# This file is part of the MatrixFreeNewton.jl package
+#
+# hessianlearn is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 2.1 of the License, or any later version.
+#
+# hessianlearn is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# If not, see <http://www.gnu.org/licenses/>.
+#
+# Author: Tom O'Leary-Roseberry
+# Contact: tom.olearyroseberry@utexas.edu
+
 using Zygote
 using ElasticArrays
 using LinearAlgebra
